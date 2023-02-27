58ff03330868629e0c57d0c76e6ca4fc62251308
TaeYoonSS <lty0880@gmail.com>
2023-02-27 17:24:45 +0900

    update

---
+++ Day01/db07_python_mysql.py
@@ -0,0 +1,11 @@
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', password='12345',
+                       db='homeplus',charset='utf8')
+
+cur = conn.cursor()
+sql = 'SELECT * FROM membertbl'
+cur.execute(sql)
+rows = cur.fetchall()
+print(rows)
+conn.close() # DB는 접속후 반드시 닫아줘야 함
