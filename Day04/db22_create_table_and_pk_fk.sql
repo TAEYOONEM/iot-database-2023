f3cebc462bff664e041067ab73acb4b0ab8e16d7
TaeYoonSS <lty0880@gmail.com>
2023-03-06 09:13:11 +0900

    Update db22_create_table_and_pk_fk.sql

--- Day04/db22_create_table_and_pk_fk.sql
+++ Day04/db22_create_table_and_pk_fk.sql
@@ -23,3 +23,17 @@ CREATE TABLE prodtbl(
     CONSTRAINT pk_prodtbl_prodCode_prodId
 		PRIMARY KEY (prodCode, prodID)
 );
+
+-- 멤버테이블
+CREATE TABLE membertbl(
+	userID CHAR(8) NOT NULL PRIMARY KEY,
+    name VARCHAR(10) NOT NULL,
+    email VARCHAR(50) NULL UNIQUE -- 유니크 제약조건
+);
+
+CREATE TABLE stdtbl(
+	userID CHAR(8) NOT NULL PRIMARY KEY,
+	name VARCHAR(10),
+    grade INT CHECK (grade >= 1 AND grade <= 4), -- 학년은 1~4
+    CONSTRAINT CK_name CHECK (name is NOT NULL) -- NOT NULL    
+);
